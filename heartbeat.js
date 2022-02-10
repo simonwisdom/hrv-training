23c841983281d8adeda808ebaf8e7f37152e60b3
Simon Wisdom <swisdom@squareup.com>
2022-02-10 14:14:13 +1100

    yep, another change to formula..

--- heartbeat.js
+++ heartbeat.js
@@ -316,8 +316,9 @@ export class Heartbeat {
         // bpmArray = [54,66,54,76,34,55,64,57,86]
         function getMeanOfSquares(array) {
           let sum = 0;
-          for (let i = 0; i < array.length; i++) {
-            sum += Math.pow(array[i], 2);
+          for (let i = 1; i < array.length; i++) {
+            rr_interval = array[i] - array[i-1];
+            sum += Math.pow(rr_interval, 2);
           }
           return Math.sqrt(sum / array.length);
         }
