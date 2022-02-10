2f33e05b28876beefca3bd66329a741c9ba24284
Simon Wisdom <swisdom@squareup.com>
2022-02-10 13:59:57 +1100

    tweaked HRV formula

--- heartbeat.js
+++ heartbeat.js
@@ -319,7 +319,7 @@ export class Heartbeat {
           for (let i = 0; i < array.length; i++) {
             sum += Math.pow(array[i], 2);
           }
-          return sum / array.length;
+          return Math.sqrt(sum);
         }
 
         const RMsSD = getMeanOfSquares(bpmArray);
